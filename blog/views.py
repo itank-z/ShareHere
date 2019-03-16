8a5f824db7d82d53ef24868b9be22586ecf81f5b
itank <ankit.it97@gmail.com>
2019-03-16 12:46:05 +0530

    Templates added and configured

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Ankit',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': '14 March 2019'
+    },
+    {
+        'author': 'itank',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': '15 March 2019'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
